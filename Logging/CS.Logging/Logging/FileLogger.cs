1d3de847ee5c7c09ae543585c008f21883d873c2
omerfatihdincer <omerfatihdincer@gmail.com>
2020-08-21 02:27:34 +0300

    Add files via upload

---
+++ Logging/CS.Logging/Logging/FileLogger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS.Logging.Logging
+{
+    public class FileLogger: ILogger
+    {
+        public FileLogger()
+        {
+
+        }
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = string.Format("{0}: {1} - {2}", logLevel.ToString(), eventId.Id, formatter(state, exception));
+            WriteMessageToFile(message);
+        }
+        private static void WriteMessageToFile(string message)
+        {
+            const string filePath = "Log\\CicekSepetiLog.txt";
+            using (var streamWriter = new StreamWriter(filePath, true))
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+    }
+}
