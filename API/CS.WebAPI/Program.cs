f00196bff33f5ec489b75e67fcd0eaed4e6d3f97
omerfatihdincer <omerfatihdincer@gmail.com>
2020-08-21 02:42:36 +0300

    Add files via upload

---
+++ API/CS.WebAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CS.EntityFrameworkCore.EntityFrameworkCore;
+using CS.EntityFrameworkCore.Seed;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CS.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var builder = new InitialDbBuilder(services.GetRequiredService<CSDbContext>());
+                    builder.Create();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .UseStartup<Startup>();
+    }
+}
---
+++ API/CS.WebAPI/Startup.cs
@@ -0,0 +1,72 @@
+﻿using CS.Business.Shared.Abstract;
+using CS.Business.Services;
+using CS.EntityFrameworkCore.Abstract;
+using CS.EntityFrameworkCore.EntityFrameworkCore;
+using CS.EntityFrameworkCore.Repositories;
+using CS.WebAPI.Infrastructure;
+using CS.WebAPI.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using AutoMapper;
+using CS.Business;
+using CS.Business.AutoMapper;
+using Microsoft.Extensions.Logging;
+using CS.Logging.Logging;
+
+namespace CS.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<CSDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:CSDbConnection"]));
+            new IoC(services).Register();
+            new CSDbConfigration(Configuration["ConnectionStrings:CSDbConnection"]);
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new CustomDtoMapper());
+            });
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            //services.addautomapper<IMapper, CustomDtoMapper>();
+            //services.AddAutoMapper(typeof(CustomDtoMapper));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ÇiçekSepeti Swagger", Version = "v1" });
+                c.OperationFilter<HeaderParameter>();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddProvider(new FileLogProvider());
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
+            });
+        }
+    }
+}
